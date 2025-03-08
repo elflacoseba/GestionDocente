8dd3bf910447d83ea9d1f3fca54f4fcc080c93ab
Sebastián Serrisuela <sebaserri@gmail.com>
2025-03-07 22:17:08 -0300

    Creado el proyecto de la aplicación Asp.Net MVC con la plantilla Able Pro. Se importaron las vistas Shared y los assets.

---
+++ GestionDocente.MVCAspNetCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionDocente.MVCAspNetCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
